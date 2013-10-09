bc6e7ed39f5de5e5a468a1c5620b58a903915b53
Jon Lazaro <lazaro.jon@gmail.com>
2013-10-09 11:55:20 +0200

    Added logger model for RDF upload tasks

--- ckanext/sparql/sparql_model/model.py
+++ ckanext/sparql/sparql_model/model.py
@@ -1,19 +1,18 @@
-from sqlalchemy import types, Column, Table, MetaData, UnicodeText, create_engine, Boolean, ForeignKey
-import vdm.sqlalchemy
+from sqlalchemy import Column, Table, MetaData, UnicodeText, Boolean, ForeignKey, DateTime
 from sqlalchemy.orm import mapper, relationship
 from ckan.model.types import make_uuid
 from ckan.model.package import Package, package_table, PACKAGE_NAME_MAX_LENGTH
 
-import sqlalchemy
+from datetime import datetime
 
 metadata = MetaData()
 
-packageendpoint_table = Table('packageendpoint', metadata,
+packageendpoint_table = Table('package_endpoint', metadata,
     Column('package_name', UnicodeText(PACKAGE_NAME_MAX_LENGTH), ForeignKey(package_table.c.name), primary_key=True),
-    Column('endpoint_id', UnicodeText, ForeignKey('sparqlendpoint.id'), primary_key=True)
+    Column('endpoint_id', UnicodeText, ForeignKey('sparql_endpoint.id'), primary_key=True)
 )
 
-sparqlendpoint_table = Table('sparqlendpoint', metadata,
+sparqlendpoint_table = Table('sparql_endpoint', metadata,
         Column('id', UnicodeText, primary_key=True, default=make_uuid),
         Column('name', UnicodeText),
         Column('sparqlurl', UnicodeText),
@@ -28,6 +27,17 @@ sparqlendpoint_table = Table('sparqlendpoint', metadata,
         Column('isenabled', Boolean, unique=False, default=True)
 )
 
+rdfuploadinglog_table = Table('rdf_uploading_log', metadata,
+    Column('id', UnicodeText, primary_key=True, default=make_uuid),
+    Column('status', UnicodeText),
+    Column('task_name', UnicodeText),
+    Column('task_id', UnicodeText),
+    Column('errors', UnicodeText),
+    Column('time', DateTime, default=datetime.now),
+    Column('endpoint_id', UnicodeText, ForeignKey('sparql_endpoint.id')),
+    Column('package_name', UnicodeText(PACKAGE_NAME_MAX_LENGTH), ForeignKey(package_table.c.name))
+)
+
 class SparqlEndpoint(object):
     def __init__(self, *initial_data, **kwargs):
         for dictionary in initial_data:
@@ -40,15 +50,12 @@ mapper(SparqlEndpoint, sparqlendpoint_table, properties={
     'packages' : relationship(Package, secondary=packageendpoint_table, backref='endpoints')
     })
 
+class RdfUploadingLog(object):
+    def __init__(self, *initial_data, **kwargs):
+        for dictionary in initial_data:
+            for key in dictionary:
+                setattr(self, key, dictionary[key])
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
 
-'''def __init__(self, name, sparqlurl, sparulurl, graph, storetype, username, passwd, isglobal, isdataallowed, isenabled):
-    self.name = name
-    self.sparqlurl = sparqlurl
-    self.sparulurl = sparulurl
-    self.graph = graph
-    self.storetype = storetype
-    self.username = username
-    self.passwd = passwd
-    self.isglobal = isglobal
-    self.isdataallowed = isdataallowed
-    self.isenabled = isenabled'''
+mapper(RdfUploadingLog, rdfuploadinglog_table)
